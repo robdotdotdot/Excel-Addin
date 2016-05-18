c06b63419d6f3851c95676a0c69a35a255fd4c9d
Robert Zhou <robdotdotdot@gmail.com>
2016-05-18 09:30:34 -0700

    Added code to set/reset hotkeys for subroutines: CustomFilterBySelection and RearrangeColumns

--- addin/hotkeys.vb
+++ addin/hotkeys.vb
@@ -1,1 +1,15 @@
 'Add-In Keyboard Shortcuts
+'If hotkeys don't work, check for conflicting/protected keys
+Sub setShortcuts()
+
+    Application.OnKey "^+c", "CustomFilterBySelection"
+    Application.OnKey "^+r", "RearrangeColumns"
+
+End Sub
+
+Sub resetShortcuts()
+
+    Application.OnKey "^c"
+    Application.OnKey "^+r"
+
+End Sub
